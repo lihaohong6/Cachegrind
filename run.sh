5946ac20f95c3b8dcdd8305728fb62a3486976a0
Haohong (Peter) Li <lih@umd.edu>
2024-02-26 18:23:12 -0500

    manually specify LL cache

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 g++ -O2 -g -o $1 $1.cpp -std=c++17 && \
   echo "Compilation done. Running" && \
   chmod 544 $1 && \
-  valgrind --tool=cachegrind --cache-sim=yes --cachegrind-out-file=cachegrind-$1.raw ./$1 && \
+  valgrind --tool=cachegrind --cache-sim=yes --LL=1048576,16,64 --cachegrind-out-file=cachegrind-$1.raw ./$1 && \
   cg_annotate --show=Dr,Dw,D1mr,D1mw,DLmr,DLmw cachegrind-$1.raw > cachegrind-$1.out && \
   code cachegrind-$1.out
