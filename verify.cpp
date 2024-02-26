4194d1e4e2f670419f85f485c771b586fd5224fe
Haohong (Peter) Li <lih@umd.edu>
2024-02-25 23:09:00 -0500

    add verifier

---
+++ verify.cpp
@@ -0,0 +1,20 @@
+#include "common.h"
+#include <cstring>
+#include <iostream>
+
+int copy[MATRIX_SIZE][MATRIX_SIZE];
+
+int main() {
+    init();
+    plain();
+    memcpy(copy, result, sizeof(copy));
+    aware();
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            if (copy[i][j] != result[i][j]) {
+                std::cout << "Mismatch at i = " << i << ", j = " << j << '\n';
+            }
+        }
+    }
+    return 0;
+}
