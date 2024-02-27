8a170fc98fd35bf8df171eb0102d1ff1840f95d1
Haohong (Peter) Li <lih@umd.edu>
2024-02-27 15:21:42 -0500

    add some documentation in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 [ $# -ne 1 ] && echo "Need an argument specifying file name" && exit 1
-g++ -O2 -g -o $1 $1.cpp -std=c++17 && \
+# use gdwarf-4 since valgrind can't read DWARF 5 debugging symbols
+g++ -gdwarf-4 -g -O2 -o $1 $1.cpp -std=c++17 && \
   echo "Compilation done. Running" && \
   chmod 544 $1 && \
-  valgrind --tool=cachegrind --cache-sim=yes --LL=1048576,16,64 --cachegrind-out-file=cachegrind-$1.raw ./$1 && \
+  # Cache size on fern: --D1=32768,8,64 --LL=35651584,17,64
+  valgrind --tool=cachegrind --cache-sim=yes --cachegrind-out-file=cachegrind-$1.raw ./$1 && \
+  # only show data cache data; ignore instructions
   cg_annotate --show=Dr,Dw,D1mr,D1mw,DLmr,DLmw cachegrind-$1.raw > cachegrind-$1.out && \
   code cachegrind-$1.out
